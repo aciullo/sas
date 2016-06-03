4304d6245447fc058d2775db357de3866ad0756e
virghy <virginiogonzalez@gmail.com>
2016-06-03 17:15:47 -0400

    Prueba GCM

--- MessageSender/Program.cs
+++ MessageSender/Program.cs
@@ -12,6 +12,7 @@ namespace MessageSender
     {
         public const string API_KEY = "AIzaSyDOmaN_SlHirOK0TSpeGWe737eo-6HOAkI";
         public const string MESSAGE = "Hello, Xamarin!";
+        
 
         static void Main(string[] args)
         {
@@ -20,6 +21,7 @@ namespace MessageSender
 
             jData.Add("message", MESSAGE);
             jGcmData.Add("to", "/topics/global");
+            //jGcmData.Add("to", "feyNemQXpNY:APA91bFdYfzvJJPIcehHaKkVjORW7WQhtOVW01QtAYV1xHojIY1Ec0rRhmd8Tx6bs7YWGXSBAIFm5I8tNv8YXXeE38OuULu12NVDLgrbgXxYJbl8-g-i50XWk-mBFsTMypv-czdXkDUS");
             jGcmData.Add("data", jData);
 
             var url = new Uri("https://gcm-http.googleapis.com/gcm/send");
@@ -37,7 +39,8 @@ namespace MessageSender
                         new StringContent(jGcmData.ToString(), Encoding.Default, "application/json"))
                             .ContinueWith(response =>
                             {
-                                Console.WriteLine(response);
+                                Console.WriteLine(response.Status);
+                                Console.WriteLine(response.Result);
                                 Console.WriteLine("Message sent: check the client device notification tray.");
                             }));
                 }
